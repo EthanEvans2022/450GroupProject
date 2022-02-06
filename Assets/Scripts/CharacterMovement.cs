8e361cf3e1e69938a6a31ff1b917bb78a3ba32bc
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-02-06 14:52:30 -0600

    Implemented Basic Character Movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private float _speed = 10;
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    private Vector3 GetNormalizedMovementDirection(){
+        Vector3 transformVector = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transformVector += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transformVector += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transformVector += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transformVector += new Vector3(0, -1, 0);
+        }
+        return transformVector.normalized;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime * GetNormalizedMovementDirection()) ;
+    }
+}
