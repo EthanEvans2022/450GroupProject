ff2eaee209cd99ea7d79670aaf586439fe3e7c0d
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-02-20 19:46:10 -0600

    Implemented dummy health system

--- Assets/Scripts/TrapController.cs
+++ Assets/Scripts/TrapController.cs
@@ -2,30 +2,20 @@ using UnityEngine;
 
 public class TrapController : MonoBehaviour
 {
-    public enum DamageType
-    {
-        None,
-        Acid,
-        Fire,
-        Lightning
-    }
-
-
     //Config
     public string[] triggerableEntities;
 
     public int damageAmount = 10;
 
-    public DamageType damageType = DamageType.None;
+    public HealthController.DamageType damageType = HealthController.DamageType.None;
 
-    //Outlets
-    private BoxCollider2D _boxCollider2D;
+
 
 
     // Start is called before the first frame update
     private void Start()
     {
-        _boxCollider2D = GetComponent<BoxCollider2D>();
+        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -33,19 +23,17 @@ public class TrapController : MonoBehaviour
         print("Trigger");
 
         foreach (var c in triggerableEntities)
-            if (col.gameObject.CompareTag(c))
+            if (col.gameObject.GetComponent<HealthController>()&&col.gameObject.CompareTag(c))
             {
-                DealDamage(col.gameObject);
+                DealDamage(col.gameObject.GetComponent<HealthController>());
                 break;
             }
     }
 
-    private void DealDamage(GameObject target)
+    private void DealDamage(HealthController target)
     {
         print("DEALING " + damageAmount + " " + damageType + " DAMAGE TO: " + target);
         
-        //Reduce hp by a certain amount if no damage type
-        
-        //Otherwise act based on damage type (give condition + do some fraction of damage)
+        target.dealDamage(damageAmount, damageType);
     }
 }
