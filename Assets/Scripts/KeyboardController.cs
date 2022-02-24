8357346382686f9be93f40dfbd74176676c3c7cf
Ethan Evans <ethanevans622@gmail.com>
2022-02-24 12:00:33 -0600

    controller: refactored mouse + updated keyboard with speed penalty

--- Assets/Scripts/KeyboardController.cs
+++ Assets/Scripts/KeyboardController.cs
@@ -14,15 +14,20 @@ public class KeyboardController : MonoBehaviour
     public Sprite seperateSprite;
     //Configurations 
     public float speed;
+    public float speedPenalty = 1;
+    /*PROTOTYPES: Not being used
     public float rotationSpeed;
     public float dashMultiplier;
+    */
     //States
-
+    private bool isCombined;
     //Methods
     void Start(){
        tf = GetComponent<Transform>(); 
        rb = GetComponent<Rigidbody2D>();
        sprite_renderer = GetComponent<SpriteRenderer>();
+       isCombined = true;
+       CombineCharacters();
     }
 
     void Update(){
@@ -43,24 +48,28 @@ public class KeyboardController : MonoBehaviour
         rb.velocity = direction.normalized * speed;
 
         //Combine Control
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C)){
+            isCombined = !isCombined;
             CombineCharacters();
-    }
-
-    protected void StandardControls(){
-        Vector2 direction = Vector2.zero;
-        rb.velocity = direction.normalized * speed;
+        }
     }
     protected void CombineCharacters(){
-        bool isActive = mousePlayer.activeSelf;
-        if(isActive){
+        if(isCombined){
+            //Deactivate mousePlayer
             mousePlayer.SetActive(false);
+            //Change current keyboard player sprite
             sprite_renderer.sprite = combinedSprite;
+            //Update speed
+            speed *= speedPenalty;
         }
         else{
+            //Activate mousePlayer
             mousePlayer.SetActive(true);
             mousePlayer.transform.position = tf.position;
+            //Change keyboard player sprite
             sprite_renderer.sprite = seperateSprite;
+            //Update speed
+            speed /= speedPenalty;
         }
     }
 
